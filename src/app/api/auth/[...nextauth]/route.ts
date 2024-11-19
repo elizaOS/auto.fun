9fccf6362e0f70d2a92f68401c4901250dd81710
TheRaizer <aidan.fu000@gmail.com>
2024-11-18 19:01:36 -0500

    add explanatory comment for weird handler syntax

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,5 @@ import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
+// https://next-auth.js.org/configuration/initialization#route-handlers-app
 export { handler as GET, handler as POST };
