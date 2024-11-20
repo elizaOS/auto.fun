899c6a934a3fb827e31c3098582a5665acd3cc3a
Derek Lance <dwalance@gmail.com>
2024-11-20 14:53:46 -0800

    fix create coin method (#17)

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -21,64 +21,53 @@ export async function createCoin(formData: {
   token_metadata: TokenMetadata;
   twitter_credentials: TwitterCredentials;
 }) {
-  return;
-
-  if (window.solana && window.solana.isPhantom) {
-    const provider = window.solana;
-
-    await provider.connect();
-    const userPublicKey = provider.publicKey;
-
-    if (!userPublicKey) {
-      throw new Error("User public key not found");
-    }
-
-    // Generate a random keypair for the token mint
-    const mintKeypair = Keypair.generate();
-
-    // call API
-    const response = await fetch(
-      "https://mint-coin.auto.fun/api/create-token",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          token_metadata: formData,
-          public_key: userPublicKey.toBase58(),
-          mint_keypair_public: mintKeypair.publicKey.toBase58(),
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
-
-    // successfully generated transaction
-    const { transaction } = await response.json();
-    console.log("got transaction");
-    const tx = VersionedTransaction.deserialize(
-      new Uint8Array(Buffer.from(transaction, "base64")),
-    );
-    console.log("deserialized transaction");
-
-    // Sign the transaction with the mint keypair
-    tx.sign([mintKeypair]);
-    console.log("signed with mint keypair");
-
-    // Request the user's signature via Phantom
-    const signedTx = await provider.signTransaction(tx);
-    console.log("signed with phantom wallet");
-
-    await fetch("https://mint-coin.auto.fun/api/submit-token-transaction", {
-      method: "POST",
-      body: JSON.stringify({
-        signed_transaction: `[${signedTx.serialize().toString()}]`,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } else {
-    // TODO: open phantom extension page in new tab
-    alert("Phantom wallet not found. Please install Phantom to proceed.");
+  if (!window.solana?.isPhantom) {
+    throw new Error("Phantom wallet not found");
   }
+  const provider = window.solana;
+
+  await provider.connect();
+  const userPublicKey = provider.publicKey;
+
+  if (!userPublicKey) {
+    throw new Error("User public key not found");
+  }
+
+  // Generate a random keypair for the token mint
+  const mintKeypair = Keypair.generate();
+
+  // call API
+  const response = await fetch("https://mint-coin.auto.fun/api/create-token", {
+    method: "POST",
+    body: JSON.stringify({
+      token_metadata: formData,
+      public_key: userPublicKey.toBase58(),
+      mint_keypair_public: mintKeypair.publicKey.toBase58(),
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  // successfully generated transaction
+  const { transaction } = await response.json();
+  const tx = VersionedTransaction.deserialize(
+    new Uint8Array(Buffer.from(transaction, "base64")),
+  );
+
+  // Sign the transaction with the mint keypair
+  tx.sign([mintKeypair]);
+
+  // Request the user's signature via Phantom
+  const signedTx = await provider.signTransaction(tx);
+
+  await fetch("https://mint-coin.auto.fun/api/submit-token-transaction", {
+    method: "POST",
+    body: JSON.stringify({
+      signed_transaction: `[${signedTx.serialize().toString()}]`,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
