3f1ec7b695367339f532012e22ff36d2ffbc4cb3
dereklance <dwalance@gmail.com>
2024-11-20 18:25:27 -0800

    update wallet button

--- src/components/common/button/RoundedButton.tsx
+++ src/components/common/button/RoundedButton.tsx
@@ -1,39 +1,37 @@
-import { useMemo } from "react";
+import { forwardRef, useMemo } from "react";
 import { RoundedButtonProps } from "../../../../types/components/common/button/RoundedButton.type";
 
-export const RoundedButton = ({
-  children,
-  variant,
-  color,
-  disabled,
-  className,
-  ...props
-}: RoundedButtonProps) => {
-  const _className = useMemo(() => {
-    if (variant == "outlined") {
-      const border = "border-solid border-[2px]";
-      switch (color) {
-        case "red":
-          return `border-[#FF0000] text-[#FF0000] bg-[rgba(255,0,0,0.15)] ${border}`;
-        default:
-          return `border-[#03ff24] text-[#03ff24] bg-[rgba(0,255,0,0.15)] ${border}`;
+export const RoundedButton = forwardRef<HTMLButtonElement, RoundedButtonProps>(
+  ({ children, variant, color, disabled, className, ...props }, ref) => {
+    const _className = useMemo(() => {
+      if (variant == "outlined") {
+        const border = "border-solid border-[2px]";
+        switch (color) {
+          case "red":
+            return `border-[#FF0000] text-[#FF0000] bg-[rgba(255,0,0,0.15)] ${border}`;
+          default:
+            return `border-[#03ff24] text-[#03ff24] bg-[rgba(0,255,0,0.15)] ${border}`;
+        }
+      } else {
+        switch (color) {
+          case "red":
+            return "text-black bg-[#FF0000]";
+          default:
+            return "text-black bg-[#03ff24]";
+        }
       }
-    } else {
-      switch (color) {
-        case "red":
-          return "text-black bg-[#FF0000]";
-        default:
-          return "text-black bg-[#03ff24]";
-      }
-    }
-  }, [variant, color]);
+    }, [variant, color]);
 
-  return (
-    <button
-      className={`${_className} rounded-xl font-bold ${disabled && "opacity-15"} active:brightness-75 ${className}`}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+    return (
+      <button
+        className={`${_className} rounded-xl font-bold ${disabled && "opacity-15"} active:brightness-75 ${className}`}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+RoundedButton.displayName = "RoundedButton";
--- src/hooks/actions/useOutsideClickDetection.ts
+++ src/hooks/actions/useOutsideClickDetection.ts
@@ -25,9 +25,9 @@ export const useOutsideClickDetection = (
       doOnOutsideClick();
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, [refs, doOnOutsideClick]);
 };
