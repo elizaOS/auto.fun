9f3e0e451c2a01c90cf4b3a6316548f6406c9d40
TheRaizer <aidan.fu000@gmail.com>
2024-11-22 15:29:13 -0500

    try padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,10 @@ export default async function RootLayout({
   const session = await getSession();
   return (
     <html lang="en" className="h-full">
-      <body
-        className={`${dmMono.className} antialiased h-full flex flex-col sm:gap-16`}
-      >
+      <body className={`${dmMono.className} antialiased h-full flex flex-col`}>
         <Providers session={session}>
           <Nav />
-          <div className="flex flex-col flex-1">{children}</div>
+          <div className="flex flex-col flex-1 pt-16">{children}</div>
         </Providers>
         <ToastContainer autoClose={5000} theme="dark" />
       </body>
