2d2a70d6ac74d9dbfd3ecb08102ff67d9a42a06e
dereklance <dwalance@gmail.com>
2024-12-17 19:15:57 -0800

    add loading skeleton

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,8 @@
 import { RoundedButton } from "@/components/common/button/RoundedButton";
 import { Token } from "./Token";
 import { useTokens } from "@/utils/tokens";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function HomePage() {
   const {
@@ -13,7 +15,65 @@ export default function HomePage() {
     nextPage,
     previousPage,
     isLiveUpdate,
+    isLoading,
   } = useTokens();
+
+  const renderSkeletons = () => (
+    <div className="grid grid-cols-3 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
+      {[...Array(30)].map((_, index) => (
+        <div
+          key={index}
+          className="px-4 pt-4 pb-5 bg-[#401141] rounded-[20px] flex flex-col gap-1"
+        >
+          <div className="self-stretch justify-between items-start inline-flex">
+            <Skeleton
+              className="!rounded-xl border border-[#642064]"
+              width={100}
+              height={100}
+              baseColor="#2b0b2c"
+              highlightColor="#521653"
+            />
+            <Skeleton
+              width={120}
+              height={24}
+              baseColor="#2b0b2c"
+              highlightColor="#521653"
+            />
+          </div>
+
+          <div className="self-stretch flex-col justify-start items-start flex gap-2">
+            <Skeleton
+              width={200}
+              height={24}
+              baseColor="#2b0b2c"
+              highlightColor="#521653"
+            />
+            <Skeleton
+              width={120}
+              height={24}
+              baseColor="#2b0b2c"
+              highlightColor="#521653"
+            />
+          </div>
+
+          <Skeleton
+            className="rounded-xl mb-[9px]"
+            width="100%"
+            height={48}
+            baseColor="#521653"
+            highlightColor="#642064"
+          />
+
+          <Skeleton
+            width={120}
+            height={24}
+            baseColor="#2b0b2c"
+            highlightColor="#521653"
+          />
+        </div>
+      ))}
+    </div>
+  );
 
   return (
     <div className="mt-12 flex flex-col">
@@ -31,23 +91,25 @@ export default function HomePage() {
         </div>
       </div>
 
-      {tokens && (
-        <div className="grid grid-cols-3 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
-          {tokens.map((token, index) => (
-            <Token
-              key={token.mint}
-              mint={token.mint}
-              marketCap="$35.6k"
-              name={token.name}
-              ticker={token.ticker}
-              url={token.image}
-              tweetUrl={token.website}
-              createdAt={token.createdAt}
-              className={`${index === 0 && isLiveUpdate ? "animate-shake" : ""}`}
-            />
-          ))}
-        </div>
-      )}
+      {isLoading
+        ? renderSkeletons()
+        : tokens && (
+            <div className="grid grid-cols-3 lg:grid-cols-2 2xl:grid-cols-3 gap-2">
+              {tokens.map((token, index) => (
+                <Token
+                  key={token.mint}
+                  mint={token.mint}
+                  marketCap="$35.6k"
+                  name={token.name}
+                  ticker={token.ticker}
+                  url={token.image}
+                  tweetUrl={token.website}
+                  createdAt={token.createdAt}
+                  className={`${index === 0 && isLiveUpdate ? "animate-shake" : ""}`}
+                />
+              ))}
+            </div>
+          )}
 
       <div className="mt-6 flex justify-center">
         <div className="flex gap-4 items-center text-white">
