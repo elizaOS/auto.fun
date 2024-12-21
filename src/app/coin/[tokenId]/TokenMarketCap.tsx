658c05d7b143b8ec6aeb0703c0209b2fc1a5ff0e
dereklance <dwalance@gmail.com>
2024-12-21 14:52:20 -0800

    add icon when bonding completed

--- src/app/coin/[tokenId]/TokenMarketCap.tsx
+++ src/app/coin/[tokenId]/TokenMarketCap.tsx
@@ -33,7 +33,7 @@ const Container = ({ children }: PropsWithChildren) => {
 
 const LoadingBar = ({ progress }: { progress: number }) => {
   return (
-    <div className="flex gap-2 items-center">
+    <div className="flex gap-2 items-center flex-1">
       <div className="w-full h-1 rounded-full bg-gray-900 flex overflow-hidden">
         <div
           className="h-full bg-[#F743F6] rounded-full z-10 shrink-0 transition-all duration-1000 ease-in-out"
@@ -105,19 +105,18 @@ export const TokenMarketCap = ({ mint }: { mint: string }) => {
     <div className="flex flex-col bg-[#401141] rounded-xl p-4 gap-3">
       <Tweet url={token.xurl} />
 
-      {token.status === "active" ? (
-        <div className="flex gap-3 w-full">
-          <Container>
-            <Title>Market cap</Title>
-            <Money>${formatNumber(token.marketCapUSD)}</Money>
-          </Container>
+      <div className="flex gap-3 w-full">
+        <Container>
+          <Title>Market cap</Title>
+          <Money>${formatNumber(token.marketCapUSD)}</Money>
+        </Container>
 
-          <Container>
-            <Title>Capital Raised</Title>
-            <Money>${formatNumber(token.liquidity)}</Money>
-          </Container>
-        </div>
-      ) : (
+        <Container>
+          <Title>Capital Raised</Title>
+          <Money>${formatNumber(token.liquidity)}</Money>
+        </Container>
+      </div>
+      {/* ) : (
         <a
           href={`https://raydium.io/swap/?inputCurrency=sol&outputMint=${token.mint}`}
           target="_blank"
@@ -125,7 +124,7 @@ export const TokenMarketCap = ({ mint }: { mint: string }) => {
         >
           Trade on Raydium
         </a>
-      )}
+      )} */}
 
       <Container>
         <div className="flex justify-between mb-3">
@@ -135,7 +134,26 @@ export const TokenMarketCap = ({ mint }: { mint: string }) => {
             position="left"
           />
         </div>
-        <LoadingBar progress={token.curveProgress} />
+        <div className="flex gap-2 items-center">
+          {token.curveProgress >= 100 && (
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.66686 3.33366H4.0002M3.33353 2.66699V4.00033M7.66686 2.66699L7.33353 4.00033M12.0002 3.33366H13.3335M12.6669 2.66699V4.00033M10.0002 6.00033L9.33353 6.66699M12.0002 8.66699L13.3335 8.33366M12.0002 12.667H13.3335M12.6669 12.0003V13.3337M9.33353 11.0123L4.9882 6.66699L2.06153 13.0537C2.00368 13.1777 1.98539 13.3165 2.00914 13.4512C2.03289 13.586 2.09753 13.7101 2.19429 13.8069C2.29104 13.9037 2.41522 13.9683 2.54998 13.992C2.68473 14.0158 2.82353 13.9975 2.94753 13.9397L9.33353 11.0123Z"
+                stroke="#F743F6"
+                strokeWidth="1.33333"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          )}
+          <LoadingBar progress={token.curveProgress} />
+        </div>
       </Container>
 
       <div className="text-[#cab7c7] text-xs font-medium leading-normal">
