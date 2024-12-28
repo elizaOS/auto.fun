128fcf35878fff8db36bef95f7f67b7fd4d444c1
TheRaizer <aidan.fu000@gmail.com>
2024-12-28 00:42:50 -0500

    add generated opengraph image

---
+++ src/utils/tokenSchema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const TokenSchema = z.object({
+  name: z.string(),
+  url: z.string(),
+  ticker: z.string(),
+  createdAt: z.string().datetime(),
+  mint: z.string(),
+  image: z.string(),
+  xurl: z.string(),
+  xname: z.string(),
+  xtext: z.string(),
+  xusername: z.string(),
+  marketCapUSD: z.number(),
+  currentPrice: z.number(),
+  curveProgress: z.number(),
+  status: z.enum([
+    "active",
+    "withdrawn",
+    "migrating",
+    "migrated",
+    "locked",
+    "migration_failed",
+  ]),
+  liquidity: z.number(),
+  curveLimit: z.number(),
+  reserveLamport: z.number(),
+  virtualReserves: z.number(),
+  solPriceUSD: z.number(),
+});
