25f9af2da3df35295743b19613a58b150e424c93
Derek Lance <derek@wombo.ai>
2025-01-17 16:02:46 -0800

    leave wallets blank so that the wallet adapter auto detects it

--- src/components/providers/WalletProvider.tsx
+++ src/components/providers/WalletProvider.tsx
@@ -2,12 +2,6 @@
 
 import { PropsWithChildren } from "react";
 import {
-  PhantomWalletAdapter,
-  SkyWalletAdapter,
-  SolflareWalletAdapter,
-  UnsafeBurnerWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import {
   ConnectionProvider,
   WalletProvider as SolanaWalletProvider,
 } from "@solana/wallet-adapter-react";
@@ -19,20 +13,9 @@ export const WalletProvider = ({
   children,
   autoConnect,
 }: PropsWithChildren<{ autoConnect: boolean }>) => {
-  const wallets =
-    process.env.NODE_ENV === "development"
-      ? // only safe in development
-        [new UnsafeBurnerWalletAdapter()]
-      : // TODO: add more wallets
-        [
-          new PhantomWalletAdapter(),
-          new SolflareWalletAdapter(),
-          new SkyWalletAdapter(),
-        ];
-
   return (
     <ConnectionProvider endpoint={env.rpcUrl}>
-      <SolanaWalletProvider wallets={wallets} autoConnect={autoConnect}>
+      <SolanaWalletProvider wallets={[]} autoConnect={autoConnect}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </SolanaWalletProvider>
     </ConnectionProvider>
