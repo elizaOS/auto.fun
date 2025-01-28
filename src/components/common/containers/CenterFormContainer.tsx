887ca701f5cf4492df72e37a340bd2990307b878
ellagoris <eduardofidera@gmail.com>
2025-01-28 02:40:30 -0300

    update form and my agent styles

--- src/components/common/containers/CenterFormContainer.tsx
+++ src/components/common/containers/CenterFormContainer.tsx
@@ -6,14 +6,12 @@ export const CenterFormContainer = ({
   submitButton,
 }: CenterFormContainerProps) => {
   return (
-    <div className="flex flex-col w-full m-auto gap-7 justify-center">
-      <div className="h-full flex flex-col items-center justify-center max-w-4xl mx-auto w-full gap-6">
-        <div className="text-left w-5/6 text-2xl">{header}</div>
-        <div className="max-h-[calc(100vh-300px)] w-5/6 rounded-[20px] border-[#03ff24] border gap-[30px] flex flex-col relative overflow-hidden">
-          <div className="w-full overflow-scroll p-6">{formComponent}</div>
-        </div>
-        {submitButton}
+    <div className="flex flex-col w-full max-w-[780px] gap-7 justify-center self-center">
+      <div className="text-left text-2xl">{header || null}</div>
+      <div className="w-full bg-[#171717] rounded-xl border border-[#03FF24]/15 gap-[30px] flex flex-col relative overflow-hidden">
+        <div className="w-full overflow-scroll p-9">{formComponent}</div>
       </div>
+      {submitButton}
     </div>
   );
 };
--- src/components/common/input/FormTextArea.tsx
+++ src/components/common/input/FormTextArea.tsx
@@ -19,7 +19,7 @@ export const FormTextArea = ({
         {rightHeaderIndicator}
       </div>
       <div
-        className={`flex flex-col items-center rounded-xl bg-[#002605] overflow-hidden relative`}
+        className={`flex flex-col items-center rounded-xl bg-[#262626] overflow-hidden relative`}
       >
         <TextareaAutosize
           className={`w-full bg-inherit h-11 py-3 px-4 resize-none placeholder-[#017d11]`}
