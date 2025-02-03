19f2d60046c6b95846f8089f8f8f124d0db86975
Derek Lance <derek@wombo.ai>
2025-02-03 14:41:55 -0800

    copy button for search result mint address

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -17,42 +17,82 @@ import { useSearchTokens } from "@/utils/tokens";
 import { formatNumber } from "@/utils/number";
 import { useOutsideClickDetection } from "@/hooks/actions/useOutsideClickDetection";
 
+const CopyButton = ({ text }: { text: string }) => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={() => {
+        navigator.clipboard.writeText(text);
+      }}
+      className="cursor-pointer"
+    >
+      <g clip-path="url(#clip0_726_6190)">
+        <path
+          d="M15 6.75H8.25C7.42157 6.75 6.75 7.42157 6.75 8.25V15C6.75 15.8284 7.42157 16.5 8.25 16.5H15C15.8284 16.5 16.5 15.8284 16.5 15V8.25C16.5 7.42157 15.8284 6.75 15 6.75Z"
+          stroke="#03FF24"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M3.75 11.25H3C2.60218 11.25 2.22064 11.092 1.93934 10.8107C1.65804 10.5294 1.5 10.1478 1.5 9.75V3C1.5 2.60218 1.65804 2.22064 1.93934 1.93934C2.22064 1.65804 2.60218 1.5 3 1.5H9.75C10.1478 1.5 10.5294 1.65804 10.8107 1.93934C11.092 2.22064 11.25 2.60218 11.25 3V3.75"
+          stroke="#03FF24"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_726_6190">
+          <rect width="18" height="18" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
 const AgentSearchResult = ({
   name,
   symbol,
   id,
   marketCap,
   imageUrl,
-  onClick,
+  onNavigate,
 }: {
   name: string;
   symbol: string;
   id: string;
   marketCap: number;
   imageUrl: string;
-  onClick: () => void;
+  onNavigate: () => void;
 }) => {
   return (
-    <Link
-      href={`/coin/${id}`}
-      className="self-stretch bg-neutral-900 flex items-center gap-6 p-2 hover:bg-[#262626] rounded-md"
-      onClick={onClick}
-    >
-      <img className="w-10 h-10 rounded-lg" src={imageUrl} alt={name} />
+    <div className="self-stretch bg-neutral-900 flex items-center gap-6 p-2 rounded-md">
+      <Link href={`/coin/${id}`} onClick={onNavigate}>
+        <img className="w-10 h-10 rounded-lg" src={imageUrl} alt={name} />
+      </Link>
       <div className="flex flex-col gap-1">
-        <div className="flex items-center gap-2">
+        <Link
+          href={`/coin/${id}`}
+          className="flex items-center gap-2"
+          onClick={onNavigate}
+        >
           <div className="text-white text-sm font-medium leading-tight">
             {name}
           </div>
           <div className="text-[#a6a6a6] text-xs font-normal uppercase leading-none tracking-widest">
             ${symbol}
           </div>
-        </div>
+        </Link>
         <div className="flex items-center gap-2">
           <div className="text-[#a6a6a6] text-xs font-normal leading-tight">
             {id.slice(0, 3)}...{id.slice(-3)}
           </div>
-          <div className="w-4 h-4 relative overflow-hidden" />
+          <CopyButton text={id} />
         </div>
       </div>
       <div className="flex items-center gap-1 ml-auto">
@@ -63,7 +103,7 @@ const AgentSearchResult = ({
           ${formatNumber(marketCap, 0)}
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
@@ -137,7 +177,7 @@ const AgentSearch = () => {
               name={token.name}
               symbol={token.ticker}
               imageUrl={token.image}
-              onClick={() => setShowSearchResults(false)}
+              onNavigate={() => setShowSearchResults(false)}
             />
           ))}
         </div>
