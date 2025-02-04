1bdba146c14ee1e3cd6385fb9a692ef37428e13a
Shaw <shawmakesmagic@gmail.com>
2025-02-03 23:53:39 -0500

    fix agents on my-agent

--- src/app/my-agents/AgentCard.tsx
+++ src/app/my-agents/AgentCard.tsx
@@ -1,6 +1,7 @@
 import { AgentSummary } from "../../../types/components/agents/index.type";
 import { AgentMedia } from "./AgentMedia";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 type AgentCardProps = Partial<AgentSummary>;
 export const AgentCard = ({
   image_src,
@@ -8,8 +9,12 @@ export const AgentCard = ({
   description,
   _id,
 }: AgentCardProps) => {
+  const router = useRouter();
   return (
-    <div className="flex flex-col bg-[#171717] p-6 rounded-xl gap-6 max-w-[420px] w-full border-solid border-[1px] border-[#03FF24]/15">
+    <div
+      className="flex flex-col bg-[#171717] p-6 rounded-xl gap-6 max-w-[420px] w-full border-solid border-[1px] border-[#03FF24]/15 cursor-pointer hover:border-[#03FF24]/50"
+      onClick={() => router.push(`/my-agents/${_id}`)}
+    >
       <div className="flex justify-between items-center">
         <div className="flex gap-3 items-center">
           <AgentMedia image_src={image_src} />
