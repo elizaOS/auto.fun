d7a5e9a034213e14526861ff755dbb64c850a372
Joey <joey.kheireddine@outlook.com>
2025-03-17 22:40:34 +0100

    upd: added all branding colors

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router";
 import { Routes, Route } from "react-router";
 import Layout from "./layout/root";
 import Homepage from "./pages";
+import Components from "./pages/components";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Homepage />} />
+          <Route path="/components" element={<Components />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,35 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#ffff",
-        secondary: "#000",
+        background: {
+          primary: "#0a0a0a",
+          card: "#171717",
+          "action-primary": "#262626",
+          disabled: "#505050",
+          input: "#262626",
+          "action-secondary": "#092f0e",
+          highlight: "#2fd345",
+          "action-highlight": "#2fd345",
+          "action-disabled": "#171717",
+        },
+        stroke: {
+          primary: "#262626",
+          highlight: "#2fd345",
+          light: "#707070",
+        },
+        text: {
+          highlight: "#2fd345",
+          primary: "#ffffff",
+          secondary: "#8c8c8c",
+          disabled: "#505050",
+          info: "#a6a6a6",
+        },
+        icon: {
+          primary: "#ffffff",
+          secondary: "#8c8c8c",
+          disabled: "#505050",
+          highlight: "#2fd345",
+        },
       },
     },
   },
