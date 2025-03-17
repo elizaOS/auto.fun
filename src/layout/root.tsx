4e916389b2808778172dd80a3b8322d0a9f64e88
Joey <joey.kheireddine@outlook.com>
2025-03-17 22:44:17 +0100

    upd: defined default border color

--- src/layout/root.tsx
+++ src/layout/root.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 export default function Layout() {
   return (
     <body>
-      <div className="min-h-screen bg-secondary flex flex-col">
+      <div className="min-h-screen bg-autofun-background-primary text-autofun-text-primary flex flex-col">
         <Header />
         <main className="flex-grow container">
           <Outlet />
