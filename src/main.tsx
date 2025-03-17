1ba564a12075fa6c61b80a74137c1457ac7906c5
Joey <joey.kheireddine@outlook.com>
2025-03-17 23:43:05 +0100

    upd: installed react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
 import { StrictMode } from "react";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { Routes, Route } from "react-router";
 import Layout from "./layout/root";
 import Homepage from "./pages";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Homepage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Homepage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
