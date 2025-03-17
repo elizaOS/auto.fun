ca76ee110a9cba98c1bbe3b06c1e9742e257654b
Joey <joey.kheireddine@outlook.com>
2025-03-17 21:50:18 +0100

    upd: created proper tailwind file so we can import brand colors

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./*.{html}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: "#ffff",
+        secondary: "#000",
+      },
+    },
+  },
+  plugins: [],
+};
