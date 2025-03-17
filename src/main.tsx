f33117c4c5f2f1ef97661d642345fdfaed6c0977
Joey <joey.kheireddine@outlook.com>
2025-03-17 21:40:17 +0100

    upd: initialized highest performance react client only setup

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { Routes, Route } from "react-router";
+import Layout from "./layout/root";
+import Page from "./pages";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Page />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
