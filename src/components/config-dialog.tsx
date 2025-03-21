42fd4c33f656e8dc38a3dfbac6b727746049271e
Joey <joey.kheireddine@outlook.com>
2025-03-22 00:09:29 +0100

    upd: created mev protection and slippage hooks

--- src/components/config-dialog.tsx
+++ src/components/config-dialog.tsx
@@ -13,9 +13,17 @@ import {
   type TTransactionSpeed,
   useTransactionSpeed,
 } from "@/hooks/use-transaction-speed";
+import { useSlippage } from "@/hooks/use-slippage";
+import {
+  type TMevProtection,
+  useMevProtection,
+} from "@/hooks/use-mev-protection";
 
 export default function ConfigDialog({ children }: PropsWithChildren) {
   const [transactionSpeed, setTransactionSpeed] = useTransactionSpeed();
+  const [slippage, setSlippage] = useSlippage();
+  const [mevProtection, setMevProtection] = useMevProtection();
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -35,32 +43,66 @@ export default function ConfigDialog({ children }: PropsWithChildren) {
             </h1>
             <XIcon className="size-5 text-autofun-icon-disabled" />
           </div>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-0.5">
-              <span className="text-base font-medium font-satoshi uppercase text-autofun-text-primary">
-                Slippage%:{" "}
-              </span>
-              <span className="font-normal text-autofun-text-highlight text-xl">
-                5.0
-              </span>
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-0.5">
+                <span className="text-base font-medium font-satoshi uppercase text-autofun-text-primary">
+                  Slippage%:{" "}
+                </span>
+                <span className="font-normal font-satoshi text-autofun-text-highlight text-xl">
+                  {Number(slippage).toFixed(1)}
+                </span>
+              </div>
             </div>
+            <p className="font-medium text-base text-autofun-text-secondary font-satoshi">
+              This is the maximum amount of slippage you are willing to accept
+              when placing trades
+            </p>
           </div>
-          <p className="font-medium text-base text-autofun-text-secondary font-satoshi">
-            This is the maximum amount of slippage you are willing to accept
-            when placing trades
-          </p>
           <Divider />
+          <div className="flex flex-col gap-3">
+            <div className="flex justify-between items-center gap-3">
+              <span className="text-base font-medium font-satoshi text-autofun-text-primary">
+                Speed
+              </span>
+              <div className="p-1 rounded-md border flex items-center gap-2">
+                {(["fast", "turbo", "ultra"] as TTransactionSpeed[]).map(
+                  (speedItem: TTransactionSpeed, _) => {
+                    const isActive = speedItem === transactionSpeed;
+                    return (
+                      <div
+                        onClick={() => setTransactionSpeed(speedItem)}
+                        className={twMerge([
+                          "py-2 px-3 capitalize text-autofun-text-secondary text-sm font-dm-mono select-none cursor-pointer tracking-[-0.4px]",
+                          isActive
+                            ? "rounded-md bg-autofun-background-action-highlight text-autofun-background-primary font-medium"
+                            : "",
+                        ])}
+                      >
+                        {speedItem}
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+            <p className="font-medium text-base text-autofun-text-secondary font-satoshi">
+              Higher speeds will increase your priority fees, making your
+              transactions confirm faster
+            </p>
+          </div>
           <div className="flex justify-between items-center gap-3">
             <span className="text-base font-medium font-satoshi text-autofun-text-primary">
               Speed
             </span>
             <div className="p-1 rounded-md border flex items-center gap-2">
-              {(["fast", "turbo", "ultra"] as TTransactionSpeed[]).map(
-                (speedItem: TTransactionSpeed, _) => {
-                  const isActive = speedItem === transactionSpeed;
+              {([true, false] as TMevProtection[]).map(
+                (mevProtectionItem: TMevProtection, _) => {
+                  const isActive = mevProtectionItem === mevProtection;
+                  const label = mevProtectionItem ? "on" : "off";
                   return (
                     <div
-                      onClick={() => setTransactionSpeed(speedItem)}
+                      onClick={() => setMevProtection(mevProtectionItem)}
                       className={twMerge([
                         "py-2 px-3 capitalize text-autofun-text-secondary text-sm font-dm-mono select-none cursor-pointer tracking-[-0.4px]",
                         isActive
@@ -68,17 +110,13 @@ export default function ConfigDialog({ children }: PropsWithChildren) {
                           : "",
                       ])}
                     >
-                      {speedItem}
+                      {label}
                     </div>
                   );
                 }
               )}
             </div>
           </div>
-          <p className="font-medium text-base text-autofun-text-secondary font-satoshi">
-            Higher speeds will increase your priority fees, making your
-            transactions confirm faster
-          </p>
         </div>
       </DialogContent>
     </Dialog>
