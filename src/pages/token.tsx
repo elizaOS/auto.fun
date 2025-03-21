23ab4769de34c5181c618f61bf17951aa6693ff1
Joey <joey.kheireddine@outlook.com>
2025-03-21 22:01:13 +0100

    upd: added social links

--- src/pages/token.tsx
+++ src/pages/token.tsx
@@ -1,13 +1,22 @@
 import Button from "@/components/button";
 import CopyButton from "@/components/copy-button";
 import Loader from "@/components/loader";
+import SkeletonImage from "@/components/skeleton-image";
 import Trade from "@/components/trade";
 import { IToken } from "@/types";
 import { abbreviateNumber, fromNow, shortenAddress } from "@/utils";
-import { getToken } from "@/utils/api";
+import { getToken, optimizePinataImage } from "@/utils/api";
+import {
+  IconBrandDiscord,
+  IconBrandDiscordFilled,
+  IconBrandTelegram,
+  IconBrandTwitter,
+  IconBrandX,
+} from "@tabler/icons-react";
 import { useQuery } from "@tanstack/react-query";
-import { ArrowUpDown } from "lucide-react";
-import { useParams } from "react-router";
+import { Globe } from "lucide-react";
+import { Link, useParams } from "react-router";
+import ShowMoreText from "react-show-more-text";
 
 export default function Page() {
   const params = useParams();
@@ -80,6 +89,41 @@ export default function Page() {
       {/* Right Section */}
       <div className="flex flex-col gap-3">
         <div className="border rounded-md p-4 bg-autofun-background-card flex flex-col gap-3">
+          <div className="flex gap-3">
+            <div className="w-36 shrink-0">
+              <SkeletonImage
+                src={optimizePinataImage(token.image, 160, 160)}
+                alt="image"
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              {/* Token Info and Time */}
+              <div className="flex items-center w-full min-w-0">
+                <div className="flex items-center gap-2 min-w-0">
+                  <div className="capitalize text-autofun-text-primary text-3xl font-medium font-satoshi leading-normal truncate min-w-0">
+                    {token.name}
+                  </div>
+                  <div className="text-autofun-text-secondary text-base font-normal font-dm-mono uppercase leading-normal tracking-widest truncate min-w-0">
+                    ${token.ticker}
+                  </div>
+                </div>
+              </div>
+              <ShowMoreText
+                /* Default options */
+                lines={2}
+                more="Show more"
+                less="Show less"
+                className="text-autofun-text-secondary text-xs font-normal font-dm-mono leading-tight min-h-8"
+                anchorClass="text-autofun-text-primary hover:text-autofun-text-highlight transition-all duration-200"
+                truncatedEndingComponent={" ... "}
+              >
+                <span className="text-autofun-text-secondary text-xs font-normal font-dm-mono leading-tight">
+                  {token.description}
+                </span>
+              </ShowMoreText>
+            </div>
+          </div>
+          {/* Contractaddress */}
           <div className="flex border rounded-md">
             <div className="size-10 rounded-l-md inline-flex border-r shrink-0 bg-autofun-background-action-primary">
               <span className="text-base font-dm-mono m-auto text-autofun-text-secondary">
@@ -91,6 +135,62 @@ export default function Page() {
                 {token?.mint}
               </span>
               <CopyButton text={token?.mint} />
+            </div>
+          </div>
+          {/* Social Links */}
+          <div className="flex items-center justify-between gap-0.5">
+            <Link to={token?.website} className="w-full" target="_blank">
+              <Button
+                className="w-full rounded-none rounded-l-md"
+                disabled={!token?.website}
+              >
+                <Globe />
+              </Button>
+            </Link>
+            <Link to={token?.twitter} className="w-full" target="_blank">
+              <Button
+                className="w-full rounded-none"
+                disabled={!token?.twitter}
+              >
+                <IconBrandX />
+              </Button>
+            </Link>
+            <Link to={token?.telegram} className="w-full" target="_blank">
+              <Button
+                className="w-full rounded-none"
+                disabled={!token?.telegram}
+              >
+                <IconBrandTelegram />
+              </Button>
+            </Link>
+            <Link to={token?.website} className="w-full" target="_blank">
+              <Button
+                className="w-full rounded-none rounded-r-md"
+                disabled={!token?.website}
+              >
+                <IconBrandDiscordFilled />
+              </Button>
+            </Link>
+          </div>
+          {/* USD Price & Solana Price */}
+          <div className="flex border rounded-md bg-autofun-background-card py-2 px-3 items-center justify-between gap-3 divide-x divide-autofun-stroke-primary">
+            <div className="flex flex-col gap-1 items-center w-full">
+              <span className="text-base font-dm-mono text-autofun-text-secondary">
+                Price USD
+              </span>
+              <span className="text-xl font-dm-mono text-autofun-text-highlight">
+                {token?.marketCapUSD
+                  ? abbreviateNumber(token?.marketCapUSD)
+                  : null}
+              </span>
+            </div>
+            <div className="flex flex-col gap-1 items-center w-full">
+              <span className="text-base font-dm-mono text-autofun-text-secondary">
+                Price
+              </span>
+              <span className="text-xl font-dm-mono text-autofun-text-primary">
+                {token?.price24hAgo ? abbreviateNumber(token?.volume24h) : null}
+              </span>
             </div>
           </div>
         </div>
