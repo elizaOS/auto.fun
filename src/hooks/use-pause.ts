f825b50139875d4908006c989351f419539513a1
Joey <joey.kheireddine@outlook.com>
2025-03-26 23:37:13 +0100

    upd: type safety on setter of setPause

--- src/hooks/use-pause.ts
+++ src/hooks/use-pause.ts
@@ -1,8 +1,6 @@
 import { useState } from "react";
 
-
-
 export default function usePause() {
-    const [pause, setPause] = useState(false);
-    return {pause, setPause}
+  const [pause, setPause] = useState<boolean>(false);
+  return { pause, setPause };
 }
