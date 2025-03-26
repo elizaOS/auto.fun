f975df50287be996e5d7c49390b9cebe5910e99e
Joey <joey.kheireddine@outlook.com>
2025-03-26 23:37:40 +0100

    upd: return array not object

--- src/hooks/use-pause.ts
+++ src/hooks/use-pause.ts
@@ -2,5 +2,5 @@ import { useState } from "react";
 
 export default function usePause() {
   const [pause, setPause] = useState<boolean>(false);
-  return { pause, setPause };
+  return [pause, setPause];
 }
