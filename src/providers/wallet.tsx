b21896c43bf0decae0745e654cebe4a25044057e
Joey <joey.kheireddine@outlook.com>
2025-03-27 01:42:54 +0100

    upd: removed not needed css

--- src/providers/wallet.tsx
+++ src/providers/wallet.tsx
@@ -6,8 +6,6 @@ import {
 
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
-import("@solana/wallet-adapter-react-ui/styles.css");
-
 export const Wallet = ({ children }: PropsWithChildren) => {
   return (
     <ConnectionProvider
