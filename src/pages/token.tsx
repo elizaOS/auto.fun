9f8154b01dcedff1885d44c19f1521e2b7579454
ajkraus04 <ajkraus04@gmail.com>
2025-04-09 10:57:01 -0400

    lint

--- src/pages/token.tsx
+++ src/pages/token.tsx
@@ -77,7 +77,7 @@ export default function Page() {
     const socket = getSocket();
 
     socket.on("updateToken", (token: any) =>
-      queryClient.setQueryData(["token", address], token)
+      queryClient.setQueryData(["token", address], token),
     );
 
     return () => {
@@ -109,7 +109,7 @@ export default function Page() {
 
         if (!hasValidData) {
           console.warn(
-            `Token page: Blockchain metrics may be invalid - all key values are 0`
+            `Token page: Blockchain metrics may be invalid - all key values are 0`,
           );
         }
 
@@ -121,7 +121,7 @@ export default function Page() {
           {
             position: "bottom-right",
             autoClose: 5000,
-          }
+          },
         );
         return null;
       }
@@ -201,7 +201,7 @@ export default function Page() {
         !metrics.marketCapUSD && !metrics.currentPrice && !metrics.volume24h;
       if (allZeros) {
         console.warn(
-          `WARNING: Blockchain metrics returned all zeros for token ${token?.mint}. This might indicate an error in data retrieval.`
+          `WARNING: Blockchain metrics returned all zeros for token ${token?.mint}. This might indicate an error in data retrieval.`,
         );
       }
     }
@@ -527,7 +527,7 @@ export default function Page() {
                     (token?.reserveLamport - token?.virtualReserves) /
                       LAMPORTS_PER_SOL,
                     true,
-                    true
+                    true,
                   )}{" "}
                   SOL in the bonding curve.
                 </p>
