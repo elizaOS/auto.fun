45344c9d2345e10599e60b27a0c264b124f5480c
Shaw <shawmakesmagic@gmail.com>
2025-04-14 19:26:49 -0700

    refresher

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,8 @@ import PageNotFound from "./pages/not-found";
 
 // if the date is before 2025-04-15, redirect to the old website
 if (
+  window.location.hostname.includes("auto.fun") &&
+  new Date() < new Date("2025-04-17T00:00:00Z") &&
   !window.location.search.includes("dev") &&
   !window.location.hostname.includes("localhost")
 ) {
