d1d35df8db371556dd20a65ddb5e23f1316c3db1
Rayan Meziani <rayanmeziani95@outlook.com>
2025-04-18 01:08:28 +0200

    lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -99,7 +99,7 @@ export default function Page() {
           onPageChange={(pageNumber: number) => {
             if (query?.isLoading) return;
             query?.goToPage(pageNumber);
-            window.scrollTo({ top: 0, behavior: 'smooth' });
+            window.scrollTo({ top: 0, behavior: "smooth" });
           }}
         />
       </div>
