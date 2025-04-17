311ad1c49d00074824134ffe787132d77e4841d3
Shaw <shawmakesmagic@gmail.com>
2025-04-17 16:56:30 -0700

    4/19

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ import PageNotFound from "./pages/not-found";
 if (
   (window.location.hostname.includes("auto.fun") ||
   window.location.hostname.includes("autofun.tech")) &&
-  new Date() < new Date("2025-04-17T00:00:00Z") &&
+  new Date() < new Date("2025-04-19T00:00:00Z") &&
   !window.location.search.includes("dev") &&
   !window.location.hostname.includes("localhost") &&
   !window.location.hostname.includes("127.0.0.1")
