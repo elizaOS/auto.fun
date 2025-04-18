10145391c3d01809a915dbb0e519b606a20ece39
ajkraus04 <ajkraus04@gmail.com>
2025-04-18 18:29:42 -0400

    Upd: make sure that owner is one claiming fees

--- src/pages/token.tsx
+++ src/pages/token.tsx
@@ -170,8 +170,8 @@ export default function Page() {
   const solanaPrice = contextSolPrice || token?.solPriceUSD || 0;
 
   const handleClaimFees = async () => {
-    if (!token?.mint) {
-      toast.error("No token ID found");
+    if (!token?.mint || token?.creator !== normalizedWallet) {
+      toast.error("No token found");
       return;
     }
 
@@ -286,7 +286,7 @@ export default function Page() {
                   isPartner
                     ? "from-autofun-background-action-highlight/10 via-autofun-background-action-highlight/10"
                     : "from-black/50 via-black/25",
-                  "absolute top-0 left-0 right-0 bg-gradient-to-b to-transparent px-3 py-2.5",
+                  "absolute top-0 left-0 right-0 bg-gradient-to-b to-transparent px-3 py-2.5"
                 )}
               >
                 <div className="flex flex-wrap items-center justify-start w-full gap-2">
@@ -537,7 +537,7 @@ export default function Page() {
                   {formatNumber(
                     tokenBalance * currentPrice * solanaPrice,
                     true,
-                    false,
+                    false
                   )}
                 </span>
               </div>
@@ -599,7 +599,7 @@ export default function Page() {
                       (token?.reserveLamport - token?.virtualReserves) /
                         LAMPORTS_PER_SOL,
                       true,
-                      true,
+                      true
                     )}{" "}
                     SOL in the bonding curve.
                   </p>
