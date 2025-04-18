10653b7c83cf12e4f9303bbffd2054b194deab9b
Shaw <shawmakesmagic@gmail.com>
2025-04-17 17:14:15 -0700

    clean up origin

--- worker/allowedOrigins.ts
+++ worker/allowedOrigins.ts
@@ -1,24 +1,33 @@
 // Define allowed origins
 
 export const allowedOrigins = [
-  "https://api-dev.auto.fun",
-  "https://api.auto.fun",
-  "https://develop.autofun.pages.dev",
-  "https://autofun.pages.dev",
-  "https://*.auto-fun.pages.dev",
-  "https://develop.auto-fun.pages.dev",
-  "https://auto-fun.pages.dev",
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://localhost:3420",
+  // auto.fun
   "https://auto.fun",
   "https://*.auto.fun",
   "https://dev.auto.fun",
   "https://develop.auto.fun",
   "https://api-dev.auto.fun",
+  "https://api.auto.fun",
+
+  // localhost
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3420",
+  "http:127.0.0.1:3000",
+
+  // autofun.pages.dev
+  "https://autofun.pages.dev",
+  "https://develop.autofun.pages.dev",
+  "https://*.autofun.pages.dev",
+
+  // auto-fun.pages.dev
+  "https://auto-fun.pages.dev",
+  "https://develop.auto-fun.pages.dev",
+  "https://*.auto-fun.pages.dev",
+
+  // autofun.tech
   "https://autofun.tech",
   "https://develop.autofun.tech",
   "https://api.autofun.tech",
   "https://api-dev.autofun.tech",
-  "https://fix-develop-create.auto-fun.pages.dev",
 ];
