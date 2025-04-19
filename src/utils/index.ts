7d8655ee91d64d64dabeda118c43f8c1c57110da
Joey <joey.kheireddine@outlook.com>
2025-04-20 01:20:02 +0200

    upd: lint

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,7 +16,7 @@ export const shortenAddress = (address: string) => {
 
 export const abbreviateNumber = (
   num: number,
-  withoutCurrency: boolean = false
+  withoutCurrency: boolean = false,
 ): string => {
   const absNum = Math.abs(Number(num));
   if (absNum < 1000) return formatNumber(num, false, withoutCurrency);
@@ -34,7 +34,7 @@ export const abbreviateNumber = (
 export const formatNumber = (
   num: number,
   showDecimals?: boolean,
-  hideDollarSign?: boolean
+  hideDollarSign?: boolean,
 ) => {
   const formatted = Intl.NumberFormat("en-US", {
     style: "currency",
@@ -51,7 +51,7 @@ export const formatNumber = (
 
 export const fromNow = (
   date: string | Date | number,
-  hideAgo?: boolean
+  hideAgo?: boolean,
 ): string => {
   const now = String(moment(date).fromNow());
   if (hideAgo) {
