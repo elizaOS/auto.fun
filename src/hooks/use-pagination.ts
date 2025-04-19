045d6cbdf1f996553564f960c6101dc04254b259
ajkraus04 <ajkraus04@gmail.com>
2025-04-19 16:13:50 -0400

    lint

--- src/hooks/use-pagination.ts
+++ src/hooks/use-pagination.ts
@@ -68,7 +68,7 @@ const fetchPaginatedData = async <
   // Validate each item in the response with the provided schema if it exists
   const validatedItems = response[itemsPropertyName]
     ? (response[itemsPropertyName] as unknown[]).map((item) =>
-        validationSchema ? validationSchema.parse(item) : (item as TOutput)
+        validationSchema ? validationSchema.parse(item) : (item as TOutput),
       )
     : [];
 
@@ -100,7 +100,7 @@ export const usePage = ({ useUrlState }: { useUrlState: boolean }) => {
         setSearchParams(newParams);
       }
     },
-    [searchParams, setSearchParams]
+    [searchParams, setSearchParams],
   );
 
   return { page, setPage: onPageChange };
@@ -203,7 +203,7 @@ export const usePagination = <TOutput extends Record<string, unknown>, TInput>({
       if (page < 1 || page > totalPages) return;
       setPage(pageNumber);
     },
-    [page, totalPages]
+    [page, totalPages],
   );
 
   const setItems = useCallback(
@@ -225,7 +225,7 @@ export const usePagination = <TOutput extends Record<string, unknown>, TInput>({
         });
       }
     },
-    [queryKey, queryClient]
+    [queryKey, queryClient],
   );
 
   return {
