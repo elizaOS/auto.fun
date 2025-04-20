f531fea29dc21e273ce180752e93ed716a5dfd28
MarcoMandar <malicemandar@gmail.com>
2025-04-20 04:30:56 +0300

    update
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- migration_be/drizzle.config.ts
+++ migration_be/drizzle.config.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+
+const DB_PATH = process.env.LOCAL_DB_PATH || '/app/data/dev.sqlite';
+
 export default defineConfig({
    out: './drizzle',
    schema: './src/db.ts',
    dialect: 'sqlite',
    dbCredentials: {
-      url: process.env.LOCAL_DB_PATH!,
+      url: `file:${DB_PATH}`,
    },
 });
