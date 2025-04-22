e12bed8f6949289c2baaa1377c31754da16bf10e
Joey <joey.kheireddine@outlook.com>
2025-04-22 16:14:38 +0200

    upd: if token is imported always prioritize it

--- src/components/token-status.tsx
+++ src/components/token-status.tsx
@@ -1,12 +1,12 @@
 import { IToken } from "@/types";
 import { twMerge } from "tailwind-merge";
 
-type TVariant = "info" | "destructive" | "warning";
+type TVariant = "info" | "destructive" | "warning" | "imported";
 
 export default function TokenStatus({ token }: { token: IToken }) {
   const status = token?.status;
 
-  const labels: Record<IToken["status"], string> = {
+  const labels: Record<IToken["status"] | "imported", string> = {
     active: "Active",
     harvested: "Harvested",
     locked: "Bonded",
@@ -16,9 +16,10 @@ export default function TokenStatus({ token }: { token: IToken }) {
     pending: "Pending",
     withdrawn: "Withdrawn",
     partner_import: "Imported",
+    imported: "Imported",
   };
 
-  const variantStatus: Record<IToken["status"], TVariant> = {
+  const variantStatus: Record<IToken["status"] | "imported", TVariant> = {
     active: "info",
     harvested: "info",
     locked: "info",
@@ -28,13 +29,17 @@ export default function TokenStatus({ token }: { token: IToken }) {
     pending: "warning",
     withdrawn: "warning",
     partner_import: "info",
+    imported: "imported",
   };
 
   const variants: Record<TVariant, string> = {
     info: "text-[#03FF24] bg-green-500/10",
     destructive: "text-red-500 bg-red-500/10",
     warning: "text-yellow-500 bg-yellow-500/10",
+    imported: "text-white bg-white/10",
   };
+
+  const isImported = Number(token?.imported) === 1;
 
   if (!status) return null;
 
@@ -42,10 +47,10 @@ export default function TokenStatus({ token }: { token: IToken }) {
     <div
       className={twMerge([
         "flex items-center justify-center p-2 text-sm font-dm-mono select-none ml-auto",
-        variants[variantStatus[status]],
+        isImported ? variants["imported"] : variants[variantStatus[status]],
       ])}
     >
-      {labels[status] || "Status Unknown"}
+      {isImported ? labels["imported"] : labels[status] || "Status Unknown"}
     </div>
   );
 }
