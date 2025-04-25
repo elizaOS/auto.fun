d940348f30139bab1da212b8ad05e350766d03ce
MarcoMandar <malicemandar@gmail.com>
2025-04-25 13:05:45 +0300

    update
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- packages/server/src/workers/migrationWorker.ts
+++ packages/server/src/workers/migrationWorker.ts
@@ -1,15 +1,13 @@
-// import { resumeMigrationsOnStart } from "../migration/resumeMigrationsOnStart";
+import { resumeMigrationsOnStart } from "../migration/resumeMigrationsOnStart";
 
-// (async () => {
-//    try {
-//       console.log(`[MigrationWorker:${process.env.NETWORK}] starting…`);
-//       await resumeMigrationsOnStart();
-//       console.log(`[MigrationWorker:${process.env.NETWORK}] finished.`);
-//       process.exit(0);
-//    } catch (err) {
-//       console.error("[MigrationWorker] error:", err);
-//       process.exit(1);
-//    }
-// })();
-
-
+(async () => {
+   try {
+      console.log(`[MigrationWorker:${process.env.NETWORK}] starting…`);
+      await resumeMigrationsOnStart();
+      console.log(`[MigrationWorker:${process.env.NETWORK}] finished.`);
+      process.exit(0);
+   } catch (err) {
+      console.error("[MigrationWorker] error:", err);
+      process.exit(1);
+   }
+})();
