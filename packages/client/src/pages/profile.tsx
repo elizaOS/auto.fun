714e0f0baa3ab28a255a45d2343474b8d98c1948
Joey <joey.kheireddine@outlook.com>
2025-05-06 16:26:10 +0200

    upd: dont use conditional

--- packages/client/src/pages/profile.tsx
+++ packages/client/src/pages/profile.tsx
@@ -61,8 +61,6 @@ const EditableProfileHeader = ({
   isSaving,
   editError,
 }: EditableProfileHeaderProps) => {
-  if (!user) return null;
-
   const displayImageUrl = editingProfilePictureUrl;
   const { adminAddresses } = env;
   const { publicKey } = useWallet();
@@ -99,6 +97,8 @@ const EditableProfileHeader = ({
       );
     },
   });
+  
+  if (!user) return null;
 
   return (
     <div className="flex flex-col lg:flex-row items-start lg:items-center gap-8 mx-auto mb-12">
