743e73bedd564bf142a540261aa3fb15ee08caee
Joey <joey.kheireddine@outlook.com>
2025-05-07 19:43:20 +0200

    upd: remove count

--- packages/server/src/middleware/rateLimiter.ts
+++ packages/server/src/middleware/rateLimiter.ts
@@ -19,7 +19,6 @@ export function createRateLimiter(redisCache: RedisCacheService) {
         const [[, count]] = (await redisCache.redisPool.useClient((client) =>
           client.multi().incr(full).expire(full, WINDOW_SEC).exec()
         )) as Array<[Error | null, number]>;
-        console.log("Rate limit count", count);
         return {
           totalHits: count,
           resetTime: new Date(Date.now() + WINDOW_SEC * 1000),
